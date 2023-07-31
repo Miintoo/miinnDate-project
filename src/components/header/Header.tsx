bd13a84d384a51f9839022cdc5344c7fa0220d8d
Minto <xpeed96@gmail.com>
2023-07-31 22:37:11 +0900

    [Feat]: header 컴포넌트 생성

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <section>
+      <div>
+        <h1>미인 데이트</h1>
+        <div>
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>Search</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </section>
+  );
+}
