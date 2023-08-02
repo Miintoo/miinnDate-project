367e9ca0678f075bd71b08fbd91fea5885fb6cbe
Minto <xpeed96@gmail.com>
2023-08-02 17:30:04 +0900

    [Feat]: main home 부분 배경 이미지 적용

--- src/page/main/Main.tsx
+++ src/page/main/Main.tsx
@@ -1,11 +1,19 @@
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header';
+import styles from './main.module.scss';
+import Home from '../../assets/home.jpg';
 
 export default function Main() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <section>
+          <div className={styles.imgContainer}>
+            <img src={Home} className={styles.home} />
+          </div>
+        </section>
+      </main>
       <Footer />
     </>
   );
