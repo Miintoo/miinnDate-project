57b09a9a151f58a9745ce9b12f130b472de4e454
Minto <xpeed96@gmail.com>
2023-08-03 17:24:39 +0900

    [Style]: react-query provider main 컴포넌트에 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Main from './page/main/Main.tsx';
 import './styles/global.scss';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Main />} />
-    </Routes>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
